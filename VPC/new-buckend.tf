faa6bbd6e81f551edf95ebc4a4252c982be75c6d
Tatiana Moraru <tatianamoraru1998@gmail.com>
2020-03-12 15:44:31 -0500

    created

---
+++ VPC/new-buckend.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-class-tatiana"
+    key    = "path/to/my/us-west-2"
+
+
+
+    region = "us-east-1"
+    #dynamodb_table = "terraform-class"
+  }
+}
