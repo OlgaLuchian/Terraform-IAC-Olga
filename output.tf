1ae303443fae07ccf8cd6b1ae260c3df445627e1
Tatiana Moraru <tatianamoraru1998@gmail.com>
2020-03-12 14:42:48 -0500

    +

---
+++ output.tf
@@ -0,0 +1,25 @@
+output "IP"{
+value = "${module.wordpress.IP}"
+}
+
+output "KEY"{
+value = "${module.wordpress.KEY}"
+}
+
+output "AMI"{
+value = "${module.wordpress.AMI}"
+}
+
+output "Sec_Group"{
+value = "${module.wordpress.Sec_Group}"
+}
+
+output "ROUTE53"{
+value = "${module.wordpress.ROUTE53}"
+
+}
+
+output "Bucketname"{
+value = "${module.wordpress.KEY}"
+}
+
