4e49d795699a4cd05898a5a91eba8c1a26fe9b26
Tatiana Moraru <tatianamoraru1998@gmail.com>
2020-03-14 20:06:20 +0000

    was created

---
+++ 3-tier-app/app.tf
@@ -0,0 +1,11 @@
+data "terraform_remote_state" "dev" {
+    data "terraform_remote_state" "main" {
+  backend = "s3"
+  config = {
+    bucket = "terraform-class-tatiana"
+    key    = "tower/us-east-1/tools/virginia/tower.tfstate"
+    region = "us-east-1"
+  }
+
+}
+}
